f1bda8eb6179973d075b12142c4fdb50df4fbee4
Hailey Sounart <sounart@kth.se>
2019-09-11 14:54:31 +0200

    Powertable and multtable functions

--- printtable.py
+++ printtable.py
@@ -1,3 +1,7 @@
+def multtable(number, limit):
+    for i in range(1, limit+1):
+        print(i * number)
+
 def powertable(number, limit):
     for i in range(1, limit+1):
         print(i ** number)
